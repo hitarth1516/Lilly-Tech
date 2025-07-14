a6afa74c1311e85dd130b44ce0016f5f9fed61be
hdave15 <150152335+hitarth1516@users.noreply.github.com>
2025-07-14 18:57:29 +0530

    Update scripts_validate_files.py

--- scripts/scripts_validate_files.py
+++ scripts/scripts_validate_files.py
@@ -314,7 +314,7 @@ def main():
 
         # Initial validation of CSV files
         files_to_validate = {
-            metric_config_path: is_valid_csv
+            master_config_path: is_valid_csv
         }
         results = {'initial_validation': {}}
         all_valid = True
